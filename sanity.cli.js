11f60f6cc91272e542b4c9f1033bbbd3949a6cfc
israrahmedjan <israrahmedjan@gmail.com>
2024-07-15 19:21:24 +0500

    skjfsj skfjsd

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pka9hpcg',
+    dataset: 'production'
+  }
+})
---
+++ schemaTypes/user.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'user',
+  title: 'Users',
+  type: 'document',
+  fields: [
+    {
+      name: 'username',
+      title: 'User Name',
+      type: 'string',
+    },
+    {
+      name: 'useremail',
+      title: 'User Email',
+      type: 'string',
+    },
+  ],
+}
+// this schema files
